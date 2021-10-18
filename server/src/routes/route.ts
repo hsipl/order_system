14c825cd19cd8f3270e055c3e96477cd2c99ddf4
p0937507934 <p0937507934@gmail.com>
2021-10-18 18:58:51 +0800

    [Basic] Merge Basic Directory struct to main (#1)

---
+++ server/src/routes/route.ts
@@ -0,0 +1,8 @@
+import MainRoute from "../bases/route.abstract";
+import BasicRoute from "./basic.route"
+// collect all the routes 
+const router: Array<MainRoute> = [
+  new BasicRoute()
+];
+
+export default router;
