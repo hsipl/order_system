d61d918f6e88f819e531bada197378546632c447
p0937507934 <p0937507934@gmail.com>
2021-10-21 17:44:16 +0800

    add eslint and prettier

---
+++ server/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  rules: {
+    "no-var": "error",
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "no-multi-spaces": "error",
+    "space-in-parens": "error",
+    "no-multiple-empty-lines": "error",
+    "no-unused-vars": ["error"],
+    camelcase: 2,
+    semi: "error",
+  },
+};
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,34 +1,39 @@
-import express from "express"
-import dotenv from "dotenv"
-import path from "path"
-import router from "./routes/route"
-import DBConnect from "./models/mysql"
-import DBConnection from "./models/mysql"
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import router from "./routes/route";
+import DBConnection from "./models/mysql";
 // create app class for server
 export class App {
-    private app:express.Application = express()
+  private app: express.Application = express();
 
-    constructor() {
-        this.setEnvironment();
-        this.setRoutes();
-        this.setDBConnection();
-    }
+  constructor() {
+    this.setEnvironment();
+    this.setRoutes();
+    this.setDBConnection();
+  }
 
-    private setEnvironment():void {
-        dotenv.config({path:path.resolve(__dirname,"./.env")})
+  private setEnvironment(): void {
+    dotenv.config({ path: path.resolve(__dirname, "./.env") });
+  }
+  private setRoutes(): void {
+    for (const route of router) {
+      this.app.use(`/api/${route.getPrefix()}`, route.getRouter());
     }
-    private setRoutes():void {
-        for (let route of router) {
-            this.app.use(`/api/${route.getPrefix()}`,route.getRouter())
-        }
-    }
-    private setDBConnection():void {
-        const db = new DBConnection("hsipl","hsipl211","127.0.0.1","3306","dev_db")
-        db.initDB()
-    }
-    public boot():void {
-        this.app.listen(process.env.PORT, () => {
-            console.log(`Server is running on ${process.env.PORT}`);
-        })
-    }
-} 
+  }
+  private setDBConnection(): void {
+    const db = new DBConnection(
+      "hsipl",
+      "hsipl211",
+      "127.0.0.1",
+      "3306",
+      "dev_db"
+    );
+    db.initDB();
+  }
+  public boot(): void {
+    this.app.listen(process.env.PORT, () => {
+      console.log(`Server is running on ${process.env.PORT}`);
+    });
+  }
+}
