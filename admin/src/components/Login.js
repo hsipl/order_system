575492efb278bc5208e5034be087ff2dada63d50
Mei-Yun-W <bear70118@gmail.com>
2022-01-03 22:31:50 +0800

    [Admin] Update login page CSS

--- admin/src/components/Login.js
+++ admin/src/components/Login.js
@@ -1,39 +1,59 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router";
 import axios from "axios";
-import styled from "styled-components";
+import styledC from "styled-components";
+import { styled } from "@mui/material/styles";
+import {
+  Dialog,
+  DialogTitle,
+  TextField,
+  Button,
+  Alert,
+  Stack,
+} from "@mui/material";
 
-const Loginform = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 500px;
-`;
-const Title = styled.h1`
-  font-weight: normal;
-`;
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
+const LoginCon = styled(Dialog)({
+  ".MuiBackdrop-root": {
+    backgroundColor: "#F3F3FA",
+  },
+});
 
-  padding: 50px;
-`;
+const Input = styled(TextField)({
+  margin: "1rem 5rem",
+  width: "40vh",
+});
 
-const Textbox = styled.input`
-  margin: 10px 0;
-  padding: 10px;
-  font-size: 20px;
-`;
-const Register = styled.a`
-  margin: 10px;
-  text-align: center;
-`;
+const Submit = styled(Button)({
+  margin: "2rem 5rem 3rem 5rem",
+});
+
+const Wrong = styled(Alert)({
+  margin: "1rem 5rem",
+});
 
 const Login = () => {
-  const [account, setAccunt] = useState("");
+  const [username, setUserName] = useState("");
   const [password, setPassword] = useState("");
-  function handleAccount(e) {
-    setAccunt(e.target.value);
+  const history = useHistory();
+  const [userLogin, setUserLogin] = useState(false);
+  const [errMes, setErrMes] = useState(false);
+
+  let config = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    withCredentials: true,
+  };
+
+  useEffect(() => {
+    if (localStorage.getItem("user-info")) {
+      history.push("/");
+    }
+  });
+
+  function handleUsername(e) {
+    setUserName(e.target.value);
     console.log(e);
   }
 
@@ -42,36 +62,92 @@ const Login = () => {
     console.log(e);
   }
 
-  async function handleSubmit(e) {
-    e.preventDefault(); 
-
+  async function login() {
+    console.warn(username, password);
+    let item = { username, password };
+    // history.push("/");
   }
 
-  return (
-    <Loginform id="login">
-      <Title>Login</Title>
-      <Form onSubmit={handleSubmit}>
-        <Textbox
-            autoFocus
-          placeholder="Account"
-          value={account}
-          onChange={handleAccount}
-          required
-        ></Textbox>
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-        <Textbox
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePassWord}
-          required
-        ></Textbox>
-        <br />
-        <Textbox type="submit" value="Sign in" ></Textbox>
-        <br />
-        <Register href="/">Don't have an account? Sign Up</Register>
-      </Form>
-    </Loginform>
+    axios
+      .post(
+        "http://localhost:8000/api/user/login",
+        { username, password },
+        {
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      )
+      .then((result) => {
+        console.log(result.data.msg);
+
+        setUserLogin(true);
+        history.push("/");
+        // console.log(result.headers['set-cookie']);
+        console.log(result.headers);
+      })
+      .catch((err) => {
+        console.log(err);
+        setUserLogin(false);
+        setErrMes(true);
+        // history.push("/")//temporary
+      });
+
+    localStorage.setItem("name", JSON.stringify(username));
+    localStorage.setItem("password", JSON.stringify(password));
+  }
+
+  const handleKeypress = (e) => {
+    if (e.keyCode === 13) {
+      handleSubmit();
+    }
+  };
+
+  return (
+    <>
+      <LoginCon open="true" maxWidth="xl">
+        <DialogTitle sx={{ fontWeight: "bold", textAlign: "center" }}>
+          登入
+        </DialogTitle>
+        <form onSubmit={handleSubmit}>
+          <Stack m={2}>
+            <Input
+              autoFocus
+              placeholder="帳號"
+              value={username}
+              onChange={handleUsername}
+              required
+              label="帳號"
+            />
+            <Input
+              id="InputPassword"
+              type="password"
+              placeholder="密碼"
+              value={password}
+              onChange={handlePassWord}
+              required
+              label="密碼"
+              onKeyPress={handleKeypress}
+            />
+            {errMes == true && <Wrong severity="warning">帳號或密碼錯誤</Wrong>}
+            <Submit
+              id="ButtonSubmit"
+              variant="contained"
+              type="submit"
+              onClick={login}
+            >
+              登入
+            </Submit>
+          </Stack>
+          {/* <Register href="/">Don't have an account? Sign Up</Register> */}
+        </form>
+      </LoginCon>
+    </>
   );
 };
 
