0c2fd65d37b0036fda22989637edc7eb99a17ba7
KE-CY <aaa01452@gmail.com>
2022-01-04 16:57:25 +0800

    [Backennd] Product seed update and Tag respository update.

--- server/src/seed/seed.ts
+++ server/src/seed/seed.ts
@@ -6,6 +6,10 @@ import { Tag } from '../entity/tag';
 import { Product } from '../entity/product';
 const genData = async () => {
   const mode = process.env.MODE ? process.env.MODE : 'default';
+  const productDeleteName = ['鹽酥雞', "雞排", "薯條", "甜不辣", "四季豆", "青椒", "香菇", "地瓜"];
+  for (let i = 0; i < 24; i++) {
+    productDeleteName.push("測資" + i.toString())
+  }
   const defaultConnection = await createConnection(mode);
   await defaultConnection
     .createQueryBuilder()
@@ -24,7 +28,7 @@ const genData = async () => {
     .createQueryBuilder()
     .delete()
     .from(Product)
-    .where('name IN (:name)', { name: ['鹽酥雞', "雞排", "薯條", "甜不辣", "四季豆", "青椒", "香菇", "地瓜"] })
+    .where('name IN (:name)', { name: productDeleteName })
     .execute();
 
   await defaultConnection
@@ -81,70 +85,79 @@ const genData = async () => {
         status: 0
       }
     ]).execute();
-  console.log('CREATE TAG SUCCESS...');
-  console.log('START PRODUCT SUCCESS...');
+  const productData = [
+    {
+      name: "鹽酥雞",
+      price: 50,
+      category: 0,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "雞排",
+      price: 60,
+      category: 0,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "薯條",
+      price: 30,
+      category: 1,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "甜不辣",
+      price: 30,
+      category: 1,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "四季豆",
+      price: 30,
+      category: 2,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "青椒",
+      price: 30,
+      category: 2,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "香菇",
+      price: 50,
+      category: 3,
+      storeId: store.identifiers[0].id,
+      status: 0
+    },
+    {
+      name: "地瓜",
+      price: 30,
+      category: 3,
+      storeId: store.identifiers[0].id,
+      status: 0
+    }
+  ];
+  for (let i = 0; i < 24; i++) {
+    productData.push({
+      name: "測資" + i.toString(),
+      price: 50,
+      category: i % 4,
+      storeId: store.identifiers[0].id,
+      status: 0
+    })
+  }
   await defaultConnection
     .createQueryBuilder()
     .insert()
     .into('product')
-    .values([
-      {
-        name: "鹽酥雞",
-        price: 50,
-        category: 0,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "雞排",
-        price: 60,
-        category: 0,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "薯條",
-        price: 30,
-        category: 1,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "甜不辣",
-        price: 30,
-        category: 1,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "四季豆",
-        price: 30,
-        category: 2,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "青椒",
-        price: 30,
-        category: 2,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "香菇",
-        price: 50,
-        category: 3,
-        storeId: store.identifiers[0].id,
-        status: 0
-      },
-      {
-        name: "地瓜",
-        price: 30,
-        category: 3,
-        storeId: store.identifiers[0].id,
-        status: 0
-      }
-    ]).execute();
+    .values(productData)
+    .execute();
   console.log('CREATE PRODUCT SUCCESS...');
   await defaultConnection.close();
 };
