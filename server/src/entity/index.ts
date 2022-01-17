ff2928e26599cb8432fe58cc50b7bbc67a72bf63
KE-CY <aaa01452@gmail.com>
2022-01-17 22:59:35 +0800

    [Backend] OrderProduct Connection

--- server/src/entity/index.ts
+++ server/src/entity/index.ts
@@ -4,11 +4,13 @@ import { User } from './user';
 import { Tag } from './tag';
 import { Product } from './product';
 import { Order } from './order';
+import { OrderProduct } from './orderProuct';
 
 export default function getConn(c: Connection) {
   User.useConnection(c);
   Store.useConnection(c);
   Tag.useConnection(c);
   Product.useConnection(c);
-  Order.useConnection(c)
+  Order.useConnection(c);
+  OrderProduct.useConnection(c)
 }
