e67d6bed4fd6efa1273e03b6b29172dd2c4bfdea
p0937507934 <p0937507934@gmail.com>
2022-01-20 23:59:03 +0800

    test script2

--- build.sh
+++ build.sh
@@ -1,19 +1,4 @@
-# DOCKER_NETWORK="order_net"
-
-# docker network create $DOCKER_NETWORK
-
-# docker run -p 3306:3306 \
-#     -e MYSQL_USER=$MYSQL_USER \
-#     -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-#     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
-#     -e MYSQL_DATABASE=$MYSQL_DATABASE \
-#     --network=$DOCKER_NETWORK \
-#     -d \
-#     mysql
-
-# docker run -p 6379:6379 --network=$DOCKER_NETWORK -d
-
-
+export MODE="dev"
 if npm list -g | grep yarn
 then 
    echo "yarn already install";
