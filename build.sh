d8dccdc9fc039d7481819f3e692ebb9f3efe1292
p0937507934 <p0937507934@gmail.com>
2022-01-20 20:54:22 +0800

    test script

--- build.sh
+++ build.sh
@@ -1,17 +1,17 @@
-DOCKER_NETWORK="order_net"
+# DOCKER_NETWORK="order_net"
 
-docker network create $DOCKER_NETWORK
+# docker network create $DOCKER_NETWORK
 
-docker run -p 3306:3306 \
-    -e MYSQL_USER=$MYSQL_USER \
-    -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-    -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
-    -e MYSQL_DATABASE=$MYSQL_DATABASE \
-    --network=$DOCKER_NETWORK \
-    -d \
-    mysql
+# docker run -p 3306:3306 \
+#     -e MYSQL_USER=$MYSQL_USER \
+#     -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
+#     -e MYSQL_PASSWORD=$MYSQL_PASSWORD \
+#     -e MYSQL_DATABASE=$MYSQL_DATABASE \
+#     --network=$DOCKER_NETWORK \
+#     -d \
+#     mysql
 
-docker run -p 6379:6379 --network=$DOCKER_NETWORK -d
+# docker run -p 6379:6379 --network=$DOCKER_NETWORK -d
 
 
 if npm list -g | grep yarn
